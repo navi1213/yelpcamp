c82ad7250027adfb4c8454e6d8088d6bda4710c5
navi1213 <timemans3@gmail.com>
2024-06-10 18:10:29 +0900

    とりあえず、CRUD操作できるようにした。エラーハンドリングはまだ

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const methodOverride = require('method-override');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true})
 .then(() => {
@@ -16,20 +18,50 @@ const app = express();
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 app.get("/",(req,res)=>{
     res.render("home");
 });
+//キャンプ場一覧
 app.get("/campgrounds",async(req,res)=>{
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index",{campgrounds});
 });
+//:idよりも先に書く（/newが:idとして認識されてしまう）
+app.get("/campgrounds/new",(req,res)=>{
+    res.render("campgrounds/new");
+});
+//キャンプ場の詳細画面への遷移
 app.get("/campgrounds/:id",async(req,res)=>{
     const {id} = req.params;
     const campground = await Campground.findById(id);
     res.render("campgrounds/show",{campground});
 });
-
+//新規作成のPOST処理
+app.post("/campgrounds",async(req,res)=>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+//編集画面への遷移
+app.get("/campgrounds/:id/edit",async(req,res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    res.render("campgrounds/edit",{campground});
+});
+//編集のPUT処理
+app.put("/campgrounds/:id",async(req,res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+app.delete("/campgrounds/:id",async(req,res)=>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+});
 app.listen("3000",()=>{
     console.log("ポート3000でサーバーを起動しました!");
 })
