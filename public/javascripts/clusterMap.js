9a8cd56203fb8fb67f9ea89ee538cde784c38524
navi1213 <timemans3@gmail.com>
2024-08-10 18:22:44 +0900

    スタイルの最終仕上げ、地図のコントロールボタンを表示

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,12 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-  container: "map",
+  container: "cluster-map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/light-v11",
   center: [138, 39],
   zoom: 3,
 });
-console.log(campgrounds);
+map.addControl(new mapboxgl.NavigationControl(),"bottom-left");
 map.on("load", () => {
   // Add a new source from our GeoJSON data and
   // set the 'cluster' option to true. GL-JS will
